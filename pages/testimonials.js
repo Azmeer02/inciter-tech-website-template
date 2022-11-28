4324e0747fbdf45728877b6e6390ea15d7cf783d
Azmeer02 <syedazmeerhaider@gmail.com>
2022-11-28 21:37:39 +0500

    completed contact-us page

---
+++ pages/testimonials.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Testimonials = () => {
+  return (
+    <div className="testimonials_container">
+      <div className="testimonials_content"></div>
+    </div>
+  );
+};
+
+export default Testimonials;
